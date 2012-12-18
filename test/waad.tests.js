c87a2f99111a97744a257b94268ac8920a48be7a
Jose F. Romaniello <jfromaniello@gmail.com>
2012-12-18 18:28:15 -0300

    refactorize tests in waad.tests.js

--- test/waad.tests.js
+++ test/waad.tests.js
@@ -1,98 +1,61 @@
 var assert = require('assert')
   , auth = require("../lib/auth")
   , Waad = require("../lib/waad")
-  , config = require('./config')
-  , waad = new Waad(/*{fiddler: true}*/);
+  , config = require('./config');
+
 
 describe('query graph', function () {
-  var access_token;
   before(function(done) {
-      auth.getAccessToken(config.TENANTID, config.APPPRINCIPALID, config.SYMMETRICKEY, function(err, token) {
-        access_token = token;
-        done(); 
-      });
+    this.tenant = config.TENANTID;
+    this.mail = 'matias@auth10dev.onmicrosoft.com';
+
+    auth.getAccessToken(config.TENANTID, config.APPPRINCIPALID, config.SYMMETRICKEY, function(err, token) {
+      this.accessToken = token;
+      done(); 
+    }.bind(this));
   });
 
-  it('should get user by email', function (done) {
-    waad.getUserByEmail(access_token, config.TENANTID, 'matias@auth10dev.onmicrosoft.com', function(err, user) {
-      assert.notEqual(null, user);
-      assert.equal('matias@auth10dev.onmicrosoft.com', user.Mail);
-      assert.equal('Matias Woloski', user.DisplayName);
-      done();
-    });
-  });
-
-  it('should return null if user not found', function (done) {
-    var waad = new Waad({tenant: config.TENANTID});
-    waad.getUserByEmail(access_token, config.TENANTID, 'nonexising@auth10dev.onmicrosoft.com', function(err, user) {
-      assert.equal(null, user);
-      done();
-    });
-  });
-
-  it('should fail if access_token is wrong', function (done) {
-    var waad = new Waad({tenant: config.TENANTID});
-    waad.getUserByEmail('wrong-token', config.TENANTID, 'nonexising@auth10dev.onmicrosoft.com', function(err) {
-      assert.notEqual(null, err);
-      assert.equal('Authentication_Unauthorized', JSON.parse(err.message).error.code);
-      done();
-    });
-  });
-
-  it('should get groups by user', function (done) {
-    var waad = new Waad({tenant: config.TENANTID});
-    waad.getGroupsForUserByEmail(access_token, config.TENANTID, 'matias@auth10dev.onmicrosoft.com', function(err, groups) {
-      assert.notEqual(null, groups);
-      assert.equal('Test Group', groups[0].DisplayName);
-      assert.equal('Company Administrator', groups[1].DisplayName);
-      done();
-    });
-  });
+  allQueryTests.bind(this)();
 });
 
 describe('query graph using token obtained with client credentials', function () {
-  var access_token2;
   before(function(done) {
-        auth.getAccessTokenWithClientCredentials(config.TENANTDOMAIN, config.APPDOMAIN, config.CLIENTID, config.CLIENTSECRET, function(err, token) {
-        access_token2 = token;
-        done();
-      });
-  });
+    
+    this.tenant = config.TENANTDOMAIN;
+    this.mail = 'matias@thesuperstore.onmicrosoft.com';
 
-  it('can get all users', function (done) {
-    waad.getUsers(access_token2, config.TENANTDOMAIN, function(err, users) {
-      assert.notEqual(null, users);
-      assert.equal('matias@thesuperstore.onmicrosoft.com', users[0].Mail);
-      assert.equal('Matias Woloski', users[0].DisplayName);
+    auth.getAccessTokenWithClientCredentials(config.TENANTDOMAIN, config.APPDOMAIN, config.CLIENTID, config.CLIENTSECRET, function(err, token) {
+      this.accessToken = token;
       done();
-    });
+    }.bind(this));
   });
 
-  it('can get all users with user groups', function (done) {
-    waad.getUsers(access_token2, config.TENANTDOMAIN, true, function(err, users) {
-      assert.equal('Test Group', users[0].groups[0].DisplayName);
-      done();
-    });
-  });
+  allQueryTests.bind(this)();
+});
 
+function allQueryTests () {
   it('should get user by email', function (done) {
-    waad.getUserByEmail(access_token2, config.TENANTDOMAIN, 'matias@thesuperstore.onmicrosoft.com', function(err, user) {
+    var waad = new Waad({tenant: this.tenant});
+    waad.getUserByEmail(this.accessToken, this.tenant, this.mail, function(err, user) {
+      if(err) return done(err);  
       assert.notEqual(null, user);
-      assert.equal('matias@thesuperstore.onmicrosoft.com', user.Mail);
+      assert.equal(this.mail, user.Mail);
       assert.equal('Matias Woloski', user.DisplayName);
       done();
-    });
+    }.bind(this));
   });
 
   it('should return null if user not found', function (done) {
-    waad.getUserByEmail(access_token2, config.TENANTDOMAIN, 'nonexising@thesuperstore.onmicrosoft.com', function(err, user) {
+    var waad = new Waad({tenant: this.tenant});
+    waad.getUserByEmail(this.accessToken, this.tenant, 'nonexising@auth10dev.onmicrosoft.com', function(err, user) {
       assert.equal(null, user);
       done();
     });
   });
 
-  it('should fail if access_token is wrong', function (done) {
-    waad.getUserByEmail('wrong-token', config.TENANTDOMAIN, 'nonexising@thesuperstore.onmicrosoft.com', function(err, user) {
+  it('should fail if this.accessToken is wrong', function (done) {
+    var waad = new Waad({tenant: this.tenant});
+    waad.getUserByEmail('wrong-token', this.tenant, 'nonexising@auth10dev.onmicrosoft.com', function(err) {
       assert.notEqual(null, err);
       assert.equal('Authentication_Unauthorized', JSON.parse(err.message).error.code);
       done();
@@ -100,7 +63,8 @@ describe('query graph using token obtained with client credentials', function ()
   });
 
   it('should get groups by user', function (done) {
-    waad.getGroupsForUserByEmail(access_token2, config.TENANTDOMAIN, 'matias@thesuperstore.onmicrosoft.com', function(err, groups) {
+    var waad = new Waad({tenant: this.tenant});
+    waad.getGroupsForUserByEmail(this.accessToken, this.tenant, this.mail, function(err, groups) {
       assert.notEqual(null, groups);
       assert.equal('Test Group', groups[0].DisplayName);
       assert.equal('Company Administrator', groups[1].DisplayName);
@@ -108,16 +72,19 @@ describe('query graph using token obtained with client credentials', function ()
     });
   });
 
-  it('should get user with groups', function (done) {
-    waad.getUserByEmail(access_token2, config.TENANTDOMAIN, 'matias@thesuperstore.onmicrosoft.com', true, function(err, user) {
-      assert.notEqual(null, user);
-      assert.equal('matias@thesuperstore.onmicrosoft.com', user.Mail);
-      assert.equal('Matias Woloski', user.DisplayName);
-      assert.notEqual(null, user.groups);
-      assert.equal('Test Group', user.groups[0].DisplayName);
-      assert.equal('Company Administrator', user.groups[1].DisplayName);
+  it('can get all users', function (done) {
+    var waad = new Waad({tenant: this.tenant});
+    waad.getUsers(this.accessToken, this.tenant, function(err, users) {
+      if (err) return done(err);
+      assert.notEqual(null, users);
+      var length = users.filter(function(u){
+        return u.Mail === this.mail;
+      }.bind(this)).length;
+
+      assert.equal(1, length);
+      
       done();
-    });
+    }.bind(this));
   });
 
   it('should get user with groups by arbitrary property', function (done) {
@@ -132,4 +99,43 @@ describe('query graph using token obtained with client credentials', function ()
       done();
     });
   });
-});
+
+  // it('should return the skip token', function (done) {
+  //   waad.getUsers(this.accessToken, config.TENANTDOMAIN, function(err, users) {
+  //     assert.notEqual(null, users);
+  //     assert.equal('matias@thesuperstore.onmicrosoft.com', users[0].Mail);
+  //     assert.equal('Matias Woloski', users[0].DisplayName);
+  //     done();
+  //   });
+  // });
+
+  // it('can get all users with user groups', function (done) {
+  //   waad.getUsers(this.accessToken, config.TENANTDOMAIN, true, function(err, users) {
+  //     assert.equal('Test Group', users[0].groups[0].DisplayName);
+  //     done();
+  //   });
+  // });
+
+  
+  //  * this test actually fail because $skip is not implemented in the odata endpoint
+  //  * Expression method 'Skip' is not currently supported.
+   
+  // it.skip('can get all users with skip option', function (done) {
+  //   waad.getUsers(this.accessToken, config.TENANTDOMAIN, {skip: 1}, function(err, users) {
+  //     if (err) return done(err);
+  //     assert.equal(0, users.length);
+  //     done();
+  //   });
+  // });
+
+  // /*
+  //  * Unsupported query expression. Query method sequence: (OrderBy)
+  //  */
+  // it.skip('can get all users sorted', function (done) {
+  //   waad.getUsers(this.accessToken, config.TENANTDOMAIN, {orderby: 'DisplayName'}, function(err, users) {
+  //     if (err) return done(err);
+  //     assert.equal(1, users.length);
+  //     done();
+  //   });
+  // });
+}
