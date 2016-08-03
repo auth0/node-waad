63fd2cb5302732f568f672d9f0c7ef0ebdd52a62
Eduardo Diaz <eduardo.diaz@auth0.com>
2016-08-03 18:11:06 +0200

    Fix error messages in tests

--- test/auth.tests.js
+++ test/auth.tests.js
@@ -16,21 +16,21 @@ describe('login to waad', function () {
 
     it('should fail for wrong tenantId', function (done) {
         auth.getAccessToken('wrong-tenant-id', config.v1.APPPRINCIPALID, config.v1.SYMMETRICKEY, function(err, token) {
-            assert.equal('The requested namespace does not exist.', err.message);
+            assert.ok(err.message.indexOf('AADSTS90002: No service namespace named \'wrong-tenant-id\' was found in the data store.'));
             done();
         });
     });
 
     it('should fail for wrong service principal', function (done) {
         auth.getAccessToken(config.v1.TENANTID, 'wrong-principal', config.v1.SYMMETRICKEY, function(err, token) {
-            assert.ok(err.message.indexOf('ACS50027') > -1);
+            assert.ok(err.message.indexOf('AADSTS70001: Application with identifier \'wrong-principal\' was not found in the directory') > -1);
             done();
         });
     });
 
     it('should fail for wrong service key', function (done) {
         auth.getAccessToken(config.v1.TENANTID, config.v1.APPPRINCIPALID, 'wrong-key', function(err, token) {
-            assert.ok(err.message.indexOf('ACS50027') > -1);
+            assert.ok(err.message.indexOf('AADSTS70002: Error validating credentials. AADSTS50012: Client assertion contains an invalid signature.') > -1);
             done();
         });
     });
